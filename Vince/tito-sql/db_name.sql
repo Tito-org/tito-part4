2eefa75fb7032121200d65b583805a3b92ae9ea4
Vince <vincent.meoc@gmail.com>
2018-12-03 20:48:03 +0100

    modif sql

--- Vince/tito-sql/db_name.sql
+++ Vince/tito-sql/db_name.sql
@@ -1,3 +1,3 @@
-create database if not exists Tito4;
+create database if not exists Tito4DB;
 use Tito4DB;
 CREATE TABLE TitoTable (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, image VARCHAR(50) NOT NULL, comments VARCHAR(50) NOT NULL, available VARCHAR(50) NOT NULL)
