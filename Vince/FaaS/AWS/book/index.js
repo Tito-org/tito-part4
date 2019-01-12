b0e75bd33a1d7dea2834be59e613064a839bf1a8
Vincent Meoc <vincent.meoc@gmail.com>
2019-01-12 01:07:29 +0100

    change to use event.body rather than event

--- Vince/FaaS/AWS/book/index.js
+++ Vince/FaaS/AWS/book/index.js
@@ -3,7 +3,10 @@ const dbConnector = require('./db-connector');
 
 exports.handler = async (event) => {
     try{
-      var data = event;
+ //     var data = JSON.stringify(event.body);
+      var data = JSON.parse(event.body);
+      console.log("data name: "+ data.name)
+      console.log("data: "+ data)
       var str = 'UPDATE TitoTable SET available = 0 WHERE name = "' + data.name + '"';
     const results = await dbConnector(str);
     const response = {
