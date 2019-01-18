d40698ced9413958a6fa337a9db70a93e28ed93e
Vincent Meoc <vincent.meoc@gmail.com>
2019-01-18 16:55:03 +0100

    added CORS  support Headers

--- Vince/FaaS/AWS/read/index.js
+++ Vince/FaaS/AWS/read/index.js
@@ -6,6 +6,10 @@ exports.handler = async (event) => {
 		const results = await dbConnector('SELECT * FROM TitoTable');
 		const response = {
     	    statusCode: 200,
+    	    headers: {
+    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+    "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+  },
    		    body: JSON.stringify(results),
    		 };
     	return response;
