60c6bf2556015b5cb4159d3d4a04132cd4967909
Vincent Meoc <vincent.meoc@gmail.com>
2019-01-18 16:57:56 +0100

    added CORS support

--- Vince/FaaS/AWS/book/index.js
+++ Vince/FaaS/AWS/book/index.js
@@ -9,6 +9,10 @@ exports.handler = async (event) => {
     const results = await dbConnector(str);
     const response = {
           statusCode: 200,
+          headers: {
+    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+    "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+  },
           body: JSON.stringify(results),
        };
       return response;
