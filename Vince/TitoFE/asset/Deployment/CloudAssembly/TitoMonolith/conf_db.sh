25ce57d7e376d89d13f7e37fef0180a57af10e6b
Vincent Meoc <vincent.meoc@gmail.com>
2019-01-30 11:56:51 +0100

    Update conf_db.sh

--- Vince/TitoFE/asset/Deployment/CloudAssembly/TitoMonolith/conf_db.sh
+++ Vince/TitoFE/asset/Deployment/CloudAssembly/TitoMonolith/conf_db.sh
@@ -18,5 +18,8 @@ SET PASSWORD for root@'%' = PASSWORD('$db_password');
 SET PASSWORD for root@'localhost' = PASSWORD('$db_password');
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+create database if not exists TitoDB;
+use TitoDB;
+CREATE TABLE TitoTable (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, home VARCHAR(50) NOT NULL, work VARCHAR(50) NOT NULL, hour_home_departure VARCHAR(50) NOT NULL, hour_work_departure VARCHAR(50) NOT NULL);
 EOF
 
