2d5bb7b1786a745006b4c382589c221405300fda
Vincent Meoc <vincent.meoc@gmail.com>
2019-01-30 11:18:35 +0100

    update TitoDB creation

--- Vince/TitoFE/asset/Deployment/CloudAssembly/TitoMonolith/conf_db.sh
+++ Vince/TitoFE/asset/Deployment/CloudAssembly/TitoMonolith/conf_db.sh
@@ -1,12 +1,25 @@
+#this script install mysql and do a minimal set of customization
+exec &> logfile.txt
+
+##### Variables
+db_password=Tito2016
+
+echo
+echo "####### BIND VARIABLES #######"
+echo "db_password=$db_password" > tito.log 
+
 service mariadb start
 
 #Conf MySql
 mysql -u root <<EOF
 FLUSH PRIVILEGES;
-CREATE USER 'root'@'%' IDENTIFIED BY 'Tito2016';
-SET PASSWORD for root@'%' = PASSWORD('Tito2016');
-SET PASSWORD for root@'localhost' = PASSWORD('Tito2016');
+CREATE USER 'root'@'%' IDENTIFIED BY '$db_password';
+SET PASSWORD for root@'%' = PASSWORD('$db_password');
+SET PASSWORD for root@'localhost' = PASSWORD('$db_password');
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+create database if not exists TitoDB;
+use TitoDB;
+CREATE TABLE TitoTable (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, home VARCHAR(50) NOT NULL, work VARCHAR(50) NOT NULL, hour_home_departure VARCHAR(50) NOT NULL, hour_work_departure VARCHAR(50) NOT NULL);
 EOF
 
