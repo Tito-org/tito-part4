bc997fa3bfa0ec2ea1e73314f66bfb40f37b521b
Vincent Meoc <vincent.meoc@gmail.com>
2019-02-12 15:49:12 +0100

    removed useless rep. Updated CAS BP with layout

--- Vince/tito-sql/db_name.sql
+++
@@ -1,5 +0,0 @@
-create database if not exists Tito4DB;
-use Tito4DB;
-CREATE TABLE TitoTable (name VARCHAR(50) PRIMARY KEY, picture VARCHAR(2000) NOT NULL, capa INT(3), location VARCHAR(3) NOT NULL, available INT(1));
-INSERT INTO TitoTable VALUES("Audi Q7","https://s3-eu-west-1.amazonaws.com/tito4picture/2018-audi-q7-20-tfsi-quattro-komfort.png",70,0.2,1);
-INSERT INTO TitoTable VALUES("Tesla Model S"," https://s3-eu-west-1.amazonaws.com/tito4picture/tesla2.png",88,0.1,0);
